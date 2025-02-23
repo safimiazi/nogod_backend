377d4e131e84172a8752685cb3e833245661b114
safi miazi <mohibullamiazi@gmail.com>
2025-02-23 11:18:02 +0600

    refactor student service update logic to simplify payload handling and remove commented code

--- src/app/modules/Student/student.service.ts
+++ src/app/modules/Student/student.service.ts
@@ -38,42 +38,22 @@ const getSingleStudentFromDB = async (id: string) => {
 };
 
 const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
-  const { name, guardian, localGuardian, ...remainingStudentData } = payload;
+  // const { name,  ...remainingStudentData } = payload;
 
-  const modifiedUpdatedData: Record<string, unknown> = {
-    ...remainingStudentData,
-  };
+  // const modifiedUpdatedData: Record<string, unknown> = {
+  //   ...remainingStudentData,
+  // };
 
-  /*
-    guardain: {
-      fatherOccupation:"Teacher"
-    }
 
-    guardian.fatherOccupation = Teacher
 
-    name.firstName = 'Mezba'
-    name.lastName = 'Abedin'
-  */
+  // if (name && Object.keys(name).length) {
+  //   for (const [key, value] of Object.entries(name)) {
+  //     modifiedUpdatedData[`name.${key}`] = value;
+  //   }
+  // }
 
-  if (name && Object.keys(name).length) {
-    for (const [key, value] of Object.entries(name)) {
-      modifiedUpdatedData[`name.${key}`] = value;
-    }
-  }
 
-  if (guardian && Object.keys(guardian).length) {
-    for (const [key, value] of Object.entries(guardian)) {
-      modifiedUpdatedData[`guardian.${key}`] = value;
-    }
-  }
-
-  if (localGuardian && Object.keys(localGuardian).length) {
-    for (const [key, value] of Object.entries(localGuardian)) {
-      modifiedUpdatedData[`localGuardian.${key}`] = value;
-    }
-  }
-
-  const result = await Student.findByIdAndUpdate(id, modifiedUpdatedData, {
+  const result = await Student.findByIdAndUpdate(id, payload, {
     new: true,
     runValidators: true,
   });

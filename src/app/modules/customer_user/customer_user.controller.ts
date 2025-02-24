5f7658d7a38e6af827d921a476071644c3897508
safi miazi <mohibullamiazi@gmail.com>
2025-02-25 02:40:52 +0600

    refactor cash-out functionality; implement async error handling and improve response structure

--- src/app/modules/customer_user/customer_user.controller.ts
+++ src/app/modules/customer_user/customer_user.controller.ts
@@ -3,6 +3,7 @@ import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { NextFunction, Request, Response } from 'express';
 import { CustomerUserServices } from './customer_user.service';
+import catchAsync from '../../utils/catchAsync';
 
 const UserSendMoneyToUser = async (
   req: Request,
@@ -28,31 +29,25 @@ const UserSendMoneyToUser = async (
   }
 };
 
+export const UserCashOut = catchAsync(async (req: Request, res: Response) => {
+  const { agentPhone, amount, accountPin } = req.body;
+  const { mobile: userPhone } = req.user;
+  const result = await CustomerUserServices.UserCashOutIntodb(
+    userPhone,
+    agentPhone,
+    amount,
+    accountPin,
+  );
 
-const UserCashOut = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { amount, mobile: receiverPhone } = req.body;
-    const { mobile: senderPhone } = req.user;
-    const result = await CustomerUserServices.UserSendMoneyToUserIntoDb(
-      senderPhone,
-      receiverPhone,
-      amount,
-    );
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Money is sent successfully!',
-      data: result,
-    });
-  } catch (error: any) {
-    next(error);
-  }
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Cash-out transaction completed successfully.',
+    data: result,
+  });
+});
 
 export const CustomerUserControllers = {
-  UserSendMoneyToUser, UserCashOut
+  UserSendMoneyToUser,
+  UserCashOut,
 };
